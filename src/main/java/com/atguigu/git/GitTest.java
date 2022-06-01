c1a2c1b28246877c430083385242bcb558001dd1
grape <grape@zhangqiang.com>
2022-06-01 12:54:04 +0800

    merge-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,7 +5,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git-hot-fix"); }
+        System.out.println("hello git-hot-fix");
         System.out.println("hello git-hot-fix-test");
         System.out.println("hello git hot-hot");
+    }
 }
