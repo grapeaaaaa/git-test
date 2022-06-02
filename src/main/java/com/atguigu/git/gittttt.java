5565a8144d172602a6240e9f41f9f6fb3e532cf4
grape <grape@zhangqiang.com>
2022-06-02 19:16:24 +0800

    for runxi commit

--- src/main/java/com/atguigu/git/gittttt.java
+++ src/main/java/com/atguigu/git/gittttt.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class gittttt {
     public static void main(String[] args) {
         System.out.println("for chunmeng");
+        System.out.println("for runxi");
     }
 }
