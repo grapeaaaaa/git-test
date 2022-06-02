92981e0df770d7f4f8ded423ddccd8f0ff9aaa8a
grape <grape@zhangqiang.com>
2022-06-02 19:39:27 +0800

    for sb commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git-hot-fix");
         System.out.println("hello git hot-hot");
+        System.out.println("for sb");
     }
 }
