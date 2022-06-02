28695e53578c0e2740c8a0e93c53184a9737ab83
grape <grape@zhangqiang.com>
2022-06-02 19:16:51 +0800

    for qiannan commit

--- src/main/java/com/atguigu/git/gittttt.java
+++ src/main/java/com/atguigu/git/gittttt.java
@@ -4,5 +4,6 @@ public class gittttt {
     public static void main(String[] args) {
         System.out.println("for chunmeng");
         System.out.println("for runxi");
+        System.out.println("for qiannan");
     }
 }
