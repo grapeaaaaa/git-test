a5d9addddee0795bece47a20faacb18c393eed70
grape <grape@zhangqiang.com>
2022-06-02 19:15:34 +0800

    for chunmeng commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git push test");
         System.out.println("hello git push test2");
         System.out.println("hello git push_last test");
+        System.out.println("for chunmeng");
     }
 }
--- src/main/java/com/atguigu/git/gittttt.java
+++ src/main/java/com/atguigu/git/gittttt.java
@@ -1,4 +1,7 @@
 package com.atguigu.git;
 
 public class gittttt {
+    public static void main(String[] args) {
+        System.out.println("for chunmeng");
+    }
 }
